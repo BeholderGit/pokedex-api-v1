8bd31af01c7bba1b746b40286a9cde212353eb03
BeholderGit <beholderer@gmail.com>
2022-10-04 14:16:34 +0200

    working on the fetch api

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const url = "https://pokeapi.co/api/v2/pokemon?limit=151";
+const pokemon_container = document.getElementsByClassName("pokemon-container");
 const colors = {
     fire: "#FDDFDF",
     grass: "#DEFDE0",
@@ -20,6 +21,20 @@ const fetchPokemons = async () => {
     const res = await fetch(url);
     const data = await res.json();
     console.log(data);
+    const pokemons = data.results;
+    pokemons.forEach(async (pokemon) => {
+        createPokemonCard(pokemon);
+    });
+};
+
+const createPokemonCard = async (pokemon) => {
+    const pokemons = await fetch(pokemon.url);
+    const pokemonData = await pokemons.json();
+    console.log(pokemonData);
+    const pokemon_div = document.createElement("div");
+    console.log(pokemon_div);
+    pokemon_div.classList.add("pokemon");
+    pokemon_container.appendChild(pokemon_div);
 };
 
 fetchPokemons();
